366e0f27dccd18728da6d0e7bccf9b06cab4b263
ctrix <mcetra@gmail.com>
2017-12-27 09:58:21 +0000

    Adding a few additional wrappers

---
+++ deb-automate-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+# This script is an utility to automate the building of the
+# kafka package.
+
+apt-get -y install dpkg-dev debhelper dh-systemd devscripts
+
+rm -f ../kafka*.orig.tar.gz
+uscan -v --download-current-version
+FILE=`ls -1 ../kafka*.orig.tar.gz`
+tar zxvf ../kafka_1.0.0.orig.tar.gz --strip 1
+
+dpkg-buildpackage -us -uc -b
