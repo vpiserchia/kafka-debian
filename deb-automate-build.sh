e1414a3e78086a1329b9959863cb587cf0afe0ed
ctrix <mc@spamteq.com>
2018-11-26 13:01:10 +0000

    Smaller dependencies

--- deb-automate-build.sh
+++ deb-automate-build.sh
@@ -5,11 +5,12 @@
 # This script is an utility to automate the building of the
 # kafka package.
 
-sudo apt-get -y install dpkg-dev debhelper dh-systemd devscripts libwww-perl
+sudo apt-get --no-install-recommends --no-install-suggests \
+    -y install dpkg-dev debhelper dh-systemd devscripts libwww-perl gnupg2
 
 rm -f ../kafka*.orig.tar.gz
 uscan -v --download-current-version
 FILE=`ls -1 ../kafka*.orig.tar.gz`
 tar zxvf ../kafka_*.orig.tar.gz --strip 1
 
-dpkg-buildpackage -us -uc -b
+dpkg-buildpackage -us -uc -b --ignore-builtin-builddeps
